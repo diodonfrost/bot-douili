01122088ba32e291cd310e016815878156b34d12
diodonfrost <diodon.frost@diodonfrost.me>
2020-04-12 20:00:21 +0200

    removing unsued import

--- douili/bot.py
+++ douili/bot.py
@@ -1,8 +1,9 @@
-"""Discord bot Douili main entry point. """
+# -*- coding: utf-8 -*-
+
+"""Discord bot Douili main entry point."""
 import os
 from random import randint
 
-import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -10,17 +11,17 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 BOT_ID = os.getenv("DISCORD_BOT_ID")
-bot = commands.Bot(command_prefix=str(BOT_ID) + " ")
+bot = commands.Bot(command_prefix=BOT_ID)
 
 
-@bot.command(name="99", help="Douili douili douili")
+@bot.command(name=" 99", help="Douili douili douili")
 async def douili(ctx):
     """Test Discord command."""
     response = randint(1, 8) * "douili "
     await ctx.send(response)
 
 
-@bot.command(name="minecraft", help="Minecraft server")
+@bot.command(name=" minecraft", help="Minecraft server")
 async def create_minecraft(ctx):
     """Test Discord command."""
     response = randint(1, 8) * "douili "
